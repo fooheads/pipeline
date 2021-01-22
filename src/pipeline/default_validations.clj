6789d92350ce399f282d294d1328fc61a4ea73c4
Niclas Nilsson <niclas@niclasnilsson.se>
2021-01-22 13:16:58 +0200

    Make refs to validation fns vars.
    In order to serialize/deserialize pipelines better, make the references
    vars.

--- src/pipeline/default_validations.clj
+++ src/pipeline/default_validations.clj
@@ -2,14 +2,14 @@
   (:require
     [clojure.spec.alpha :as s]))
 
-(defn- valid? [context spec v]
+(defn valid? [context spec v]
   (s/valid? spec v))
 
-(defn- explain-data [context spec v]
+(defn explain-data [context spec v]
   (s/explain-data spec v))
 
 (def default-validation-fns
-  {:valid? valid?
-   :explain explain-data})
+  {:valid? #'valid?
+   :explain #'explain-data})
 
 
